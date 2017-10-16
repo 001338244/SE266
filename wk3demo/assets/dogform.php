8172e61432b3d29d2b3909dbb25cf04040675523
Cooke <jecooke@labs.neit.edu>
2017-10-16 13:49:54 -0400

    Add Dog

---
+++ wk3demo/assets/dogform.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 001338244
+ * Date: 10/16/2017
+ * Time: 1:15 PM
+ */
+?>
+<form method="post" action="#">
+    Name: <input type="text" name="name" /><br />
+    Gender: M <input type="radio" name="gender" value="M" />
+    F <input type="radio" name="gender" value="M" /><br />
+    Fixed: <input type="checkbox" name="fixed" value="true" /><br />
+    <input type="submit" name="action" value="Add" />
+</form>
