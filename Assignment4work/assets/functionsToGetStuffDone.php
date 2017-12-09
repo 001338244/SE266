5b84309255f5451936ee4d8c4a8af8c90455ab23
001338244 <31701989+001338244@users.noreply.github.com>
2017-12-08 23:56:14 -0500

    Add files via upload
    did not have the time to get add working

--- Assignment4work/assets/functionsToGetStuffDone.php
+++ Assignment4work/assets/functionsToGetStuffDone.php
@@ -5,45 +5,163 @@
  * Date: 10/23/2017
  * Time: 11:55 AM
  */
-function getCorpAsTable($db)
+function getCorpsAsTable($db, $colNames, $col = null, $sortable = false)
 {
-    try {
-        $sql = $db->prepare("SELECT * FROM corps");
-        $sql->execute();
-        $phpclassfall2017 = $sql->fetchAll(PDO::FETCH_ASSOC);
-        //$aff_id = $db ->prepare("SELECT id FROM corps");
-
-
-        //tr is table row     td is table data
-        if ( $sql->rowCount() > 0 ) {
-            $table = "<table>" . PHP_EOL;
-            foreach ($phpclassfall2017 as $corpz) {
-                $table .= "<tr><td>" . $corpz['corp'];
-                $table .= "<a href=\"view.php?id=" . $corpz['id'] . "\"> View </a>";
-                $table .= "<a href=\"update.php?id=" . $corpz['id'] . "\"> Update </a>";
-                $table .= "<a href=\"delete.php?id=" . $corpz['id'] . "\"> Delete </a>";
-                $table .= "<td style='display:none'>" . $corpz['id'];
-                $table .= "</td></tr>";
-
+    $corps = getCorpsAsSortedTable($db, $col, $sortable);
+    setlocale(LC_MONETARY, 'en_US.UTF-8');
+    $table = "";
+    if ( count($corps) > 0):
+        $table .= "<table>" . PHP_EOL;
+        if ($col && ($sortable == null || $sortable == "ASC")):
+            $table .= "<tr>";
+            foreach ($colNames as $col) {
+                $sorting = "DESC";
+                $table .= "<th><a href='?action=sort&col=$col&sorting=$sorting'>$col</a></th>";
             }
-
-            $table .= "</table>" . PHP_EOL;
-
-        } else {
-            $table = "Life is sad.  There are no corps.";
-
-        }
+            $table .= "</tr>" . PHP_EOL;
+        else:
+            $table .= "<tr>";
+            foreach ($colNames as $col):
+                $sorting = "ASC";
+                $table .= "<th><a href='?action=sort&col=$col&sorting=$sorting'>$col</a></th>";
+            endforeach;
+            $table .= "</tr>" . PHP_EOL;
+        endif;
+        foreach ($corps as $corpz):
+            $table .= "\t<tr>";
+            $table .= "<td>" . $corpz['id'] . "</td>";
+            $table .= "<td>" . $corpz['corp'] . "</td>";
+            $table .= "<td>" . date('m/d/Y', strtotime($corpz['incorp_dt'])) . "</td>";
+            $table .= "<td>" . $corpz['email'] . "</td>";
+            $table .= "<td>" . $corpz['zipcode'] . "</td>";
+            $table .= "<td>" . $corpz['owner'] . "</td>";
+            $table .= "<td>" . $corpz['phone'] . "</td>";
+            $table .= "<td>" . "<a href='?id=" . $corpz['id'] . "&action=Update'> Update </a> " . "</td>";
+            $table .= "<td>" . "<a href='?id=" . $corpz['id'] . "&action=Delete'> Delete </a> " . "</td>";
+            $table .= "</tr>" . PHP_EOL;
+        endforeach;
+        $table .= "</table>" . PHP_EOL;
         return $table;
-    } catch (PDOException $e) {
-        die("There was a problem retrieving the corps");
+    else :
+        return "<section>We have no coorporations to show you</section>";
+    endif;
+}
+function getCorpsAsSortedTable($db, $colName, $sorting) {
+    if($colName == null) {
+        try {
+            $sql = "SELECT * FROM corps";
+            $stmt = $db->prepare($sql);
+            $stmt->execute();
+            $corps = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            die ("There was a problem showing the coorporations");
+        }
     }
-
-
+    else {
+        try {
+            $sql = "SELECT * FROM corps ORDER BY $colName $sorting";
+            $stmt = $db->prepare($sql);
+            $stmt->execute();
+            $corps = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            die ("There was a problem showing thew coorporations");
+        }
+    }
+    return $corps;
 }
 
-function addCorp($db, $corp, $incorp_dt, $email, $zipcode, $owner, $phone) {
+function getCorpAsUpdate($db, $id) {
     try {
-        $sql = $db->prepare("INSERT INTO corps VALUES (null, :corp, :incorp_dt, :email, :zipcode, :owner, :phone)");
+        $sql = $db->prepare("SELECT * FROM corps WHERE id = $id");
+        $sql->execute();
+        $corps = $sql->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        die ("There was a problem showing the coorporations");
+    }
+
+    setlocale(LC_MONETARY, 'en_US.UTF-8');
+    $table = "<table>";
+    foreach ($corps as $corpz):
+        $table .= "<tr>";
+        $table .= "<td>" . $corpz['id'] . "</td>";
+        $table .= "<td>" . $corpz['corp'] . "</td>";
+        $table .= "<td>" . date('m/d/Y', strtotime($corpz['incorp_dt'])) . "</td>";
+        $table .= "<td>" . $corpz['email'] . "</td>";
+        $table .= "<td>" . $corpz['zipcode'] . "</td>";
+        $table .= "<td>" . $corpz['owner'] . "</td>";
+        $table .= "<td>" . $corpz['phone'] . "</td>";
+        $table .= "</tr>" . PHP_EOL;
+        $table .= "</table>" . PHP_EOL;
+    endforeach;
+    $form = "<form method='post' action='#'>";
+    $form .= "Corporation: <input type='text' name='corp' value='" . $corpz['corp'] . "' /> <br />" . PHP_EOL;
+    $form .= "Date Incorporated: <input type='text' name='incorp_dt' value='" . $corpz['incorp_dt'] . "' /> <br />" . PHP_EOL;
+    $form .= "Email: <input type='text' name='email' value='" . $corpz['email'] . "' /> <br />" . PHP_EOL;
+    $form .= "ZipCode: <input type='text' name='zipcode' value='" . $corpz['zipcode'] . "' /> <br />" . PHP_EOL;
+    $form .= "Owner: <input type='text' name='owner' value='" . $corpz['owner'] . "' /> </br>" . PHP_EOL;
+    $form .= "Phone: <input type='text' name='phone' value='" . $corpz['phone'] . "' /> </br>" . PHP_EOL;
+    $form .= "<input type='submit' name='action' value='Update' />" . PHP_EOL;
+    $form .= "</form>";
+    return $form;
+}
+function getCorpAsDelete($db, $id)
+{
+    try {
+        $sql = $db->prepare("SELECT * FROM corps WHERE id = $id");
+        $sql->execute();
+        $corps = $sql->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        die ("There was a problem showing the cooroperations");
+    }
+
+    setlocale(LC_MONETARY, 'en_US.UTF-8');
+    $table = "<table>";
+    foreach ($corps as $corpz):
+        $table .= "<tr><td>Are you sure you want to delete this coorporation? </td>";
+        $table .= "<td>" . $corpz['id'] . "</td>";
+        $table .= "<td>" . $corpz['corp'] . "</td>";
+        $table .= "<td>" . date('m/d/Y', strtotime($corpz['incorp_dt'])) . "</td>";
+        $table .= "<td>" . $corpz['email'] . "</td>";
+        $table .= "<td>" . $corpz['zipcode'] . "</td>";
+        $table .= "<td>" . $corpz['owner'] . "</td>";
+        $table .= "<td>" . $corpz['phone'] . "</td></tr>";
+        $table .= "<tr><td>" . "<form method='post' action='#'>" . "</td></tr>";
+        $table .= "<tr><td>" . "<input type='submit' name='action' value='Delete' />" . "</td></tr>";
+    endforeach;
+    $table .= "</table>" . PHP_EOL;
+    return $table;
+}
+function getCorpByID($db, $id) {
+    try {
+        $sql = $db->prepare("SELECT * FROM corps WHERE id = $id");
+        $sql->execute();
+        $corp = $sql->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        die ("There was a problem showing the coorperatoins");
+    }
+    return $corp;
+}
+function getCorps($db) {
+    try {
+        $sql = "SELECT * FROM corps";
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+        $corp = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        die ("There was a problem showing the coorporations");
+    }
+    return $corp;
+}
+function deleteCorp($db, $id) {
+    $sql = "DELETE FROM corps WHERE id = $id";
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+}
+
+function updateCorp($db, $id, $corp, $incorp_dt, $email, $zipcode, $owner, $phone){
+    try {
+        $sql = $db->prepare("UPDATE corps SET corp = :corp, incorp_dt = :incorp_dt, email = :email, zipcode = :zipcode, owner = :owner, phone = :phone WHERE id = $id");
+        $sql->fetchAll(PDO::FETCH_ASSOC);
         $sql->bindParam(':corp', $corp);
         $sql->bindParam(':incorp_dt', $incorp_dt);
         $sql->bindParam(':email', $email);
@@ -52,43 +170,8 @@ function addCorp($db, $corp, $incorp_dt, $email, $zipcode, $owner, $phone) {
         $sql->bindParam(':phone', $phone);
         $sql->execute();
         return $sql->rowCount();
-    }catch (PDOException $e){
-        die("There was a problem giving birth to your puppy.");
-
-    }
-
-}
-
-function getCorp($db, $id)
-{
-    try {
-        $sql = $db->prepare("SELECT * FROM corps WHERE id =:id");
-        $sql->bindParam(':id', $id, PDO::PARAM_INT);
-        $sql->execute();
-        $plz = $sql->fetchAll(PDO::FETCH_ASSOC);
-
-        if ($sql->rowCount() > 0) {
-            $table = "<table>" . PHP_EOL;
-            foreach ($plz as $corpz) {
-                $table .= "<tr><td>" . $corpz['corp'];
-                $table .= "<tr><td>" . $corpz['incorp_dt'];
-                $table .= "<tr><td>" . $corpz['email'];
-                $table .= "<tr><td>" . $corpz['zipcode'];
-                $table .= "<tr><td>" . $corpz['owner'];
-                $table .= "<tr><td>" . $corpz['phone'];
-                $table .= "</td></tr>";
-
-            }
-
-            $table .= "</table>" . PHP_EOL;
-
-        } else {
-            $table = "Life is sad.  There are no corps.";
-
-        }
-        return $table;
     } catch (PDOException $e) {
-        die("There was a problem retrieving the corps");
+        die("There was a problem updating the corporation.");
     }
 }
 
@@ -99,97 +182,3 @@ function isPostRequest() {
 function isGetRequest() {
     return ( filter_input(INPUT_SERVER, 'REQUEST_METHOD') === 'GET' );
 }
-
-function updateCorp($db, $id, $corp, $incorp_dt, $email, $zipcode, $owner, $phone) {
-    try {
-        $sql = $db->prepare("UPDATE corps SET (:id, :corp, :incorp_dt, :email, :zipcode, :owner, :phone)");
-        $sql->bindParam(':id', $id);
-        $sql->bindParam(':corp', $corp);
-        $sql->bindParam(':incorp_dt', $incorp_dt);
-        $sql->bindParam(':email', $email);
-        $sql->bindParam(':zipcode', $zipcode);
-        $sql->bindParam(':owner', $owner);
-        $sql->bindParam(':phone', $phone);
-        $sql->execute();
-        return $sql->rowCount();
-    }catch (PDOException $e){
-        die("There was a problem giving birth to your puppy.");
-
-    }
-
-}
-
-function getCorps($db){
-        try {
-            $sql = "SELECT id, corp, incorp_dt, email, zipcode, owner, phone FROM corps WHERE id=id";
-            $stmt = $db->prepare($sql);
-            $stmt->execute();
-            $corps = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        } catch (PDOException $e) {
-            die ("There was a problem getting the table of employees");
-        }
-        return $corps;
-
-
-
-
-}
-
-function getCorpsAsSortedTable($db, $col, $dir){
-
-    try {
-
-
-        $sql = "SELECT id, corp, incorp_dt, email, zipcode, owner, phone FROM corps WHERE id=id ORDER BY $col, $dir";
-        $stmt = $db->prepare($sql);
-        $stmt->execute();
-        $corps = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    } catch (PDOException $e) {
-        die ("There was a problem getting the table of employees");
-    }
-    return $corps;
-
-
-}
-
-
-    function getCorpsAsTable($db, $corps, $cols = null, $sortable = false)
-    {
-        setlocale(LC_MONETARY, 'en_US.UTF-8');
-        $table = "";
-        if (count($corps) > 0):
-            $table .= "<table>" . PHP_EOL;
-            if ($cols && !$sortable):
-                $table .= "\t<tr>";
-                foreach ($cols as $col) {
-                    $table .= "<th>$col</th>"; // build column headers as anchors, indicating sort=asc&col=colname
-                }
-                $table .= "</tr>" . PHP_EOL;
-            endif;
-            if ($cols && $sortable):
-                $dir = "ASC";
-                $table .= "\t<tr>";
-                foreach ($cols as $col) {
-                    $table .= "<th><a href = '?action=sort&col=$col&dir=$dir'>$col</a></th>"; // build column headers as anchors, indicating sort=asc&col=colname
-                }
-                $table .= "</tr>" . PHP_EOL;
-            endif;
-            foreach ($corps as $corpz):
-                $table .= "\t<tr>";
-                $table .= "<td>" . $corpz['id'] . "</td>";
-                $table .= "<td>" . $corpz['corp'] . "</td>";
-                $table .= "<td>" . date('m/d/Y', strtotime($corpz['incorp_dt'])) . "</td>";
-                $table .= "<td>" . $corpz['email'] . "</td>";
-                $table .= "<td>" . $corpz['zipcode'] . "</td>";
-                $table .= "<td>" . $corpz['owner'] . "</td>";
-                $table .= "<td>" . $corpz['phone'] . "</td>";
-                $table .= "</tr>" . PHP_EOL;
-            endforeach;
-            $table .= "</table>" . PHP_EOL;
-            return $table;
-        else :
-            return "<section>We have no employees to display</section>";
-        endif;
-
-
-    }
--- Assignment4work/index.php
+++ Assignment4work/index.php
@@ -6,6 +6,56 @@
  * Time: 11:49 AM
  */
 
-include_once ("assets/header.html");
-include_once ("assets/footer.php");
-?>
+require_once ("assets/dbconn.php");
+require_once("assets/functionsToGetStuffDone.php");
+$db = dbconn();
+
+$action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING) ??
+    filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING) ?? NULL;
+$col = filter_input(INPUT_GET, 'col', FILTER_SANITIZE_STRING) ?? NULL;
+$sorting = filter_input(INPUT_GET, 'dir', FILTER_SANITIZE_STRING) ?? null;
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_STRING);
+$corp = filter_input(INPUT_POST, 'corp', FILTER_SANITIZE_STRING) ?? NULL;
+$incorp_dt = filter_input(INPUT_POST, 'incorp_dt', FILTER_SANITIZE_STRING) ?? NULL;
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING) ?? NULL;
+$zipcode = filter_input(INPUT_POST, 'zipcode', FILTER_SANITIZE_STRING) ?? NULL;
+$owner = filter_input(INPUT_POST, 'owner', FILTER_SANITIZE_STRING) ?? NULL;
+$phone = filter_input(INPUT_POST, 'phone', FILTER_SANITIZE_STRING) ?? NULL;
+switch ($action) {
+    case 'sort':
+        $title = "Sort";
+        include_once ('assets/header.html');
+        $colNames = getColumnNames($db, 'corps');
+        echo getCorpsAsTable($db, $colNames, $col, $sorting);
+        break;
+
+    case 'Read':
+        include_once ('assets/header.html');
+        break;
+
+    case 'Update':
+        $title = "Update Corp";
+        include_once ('assets/header.html');
+        echo getCorpAsUpdate($db, $id);
+        if(isPostRequest()) {
+            $form = updateCorp($db, $id, $corp, $incorp_dt, $email, $zipcode, $owner, $phone);
+            echo "Record Updated";
+        }
+        break;
+
+    case 'Delete':
+        include_once ('assets/header.html');
+        echo getCorpAsDelete($db, $id);
+        if(isPostRequest()) {
+            $table = deleteCorp($db, $id);
+            echo "Record Deleted";
+        }
+        break;
+
+    default:
+        $title = "default";
+        include_once ('assets/header.html');
+        $colNames = getColumnNames($db, 'corps');
+        echo getCorpsAsTable($db, $colNames, $col, $sorting);
+        break;
+}
